3e9b8b83857801e50f8f61b4af31a0cd8309ca11
Mladen Milankovic <developer@mmlado.com>
2023-07-12 20:14:53 +0200

    Show table containing all transactions in a block.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { Alchemy, Network } from 'alchemy-sdk';
+import { Alchemy, Network, Utils } from 'alchemy-sdk';
 import { useEffect, useState } from 'react';
+
 
 import './App.css';
 
@@ -21,16 +22,113 @@ const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [blockTransactions, setBlockTransactions] = useState([]);
 
   useEffect(() => {
     async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
+      if (!blockNumber) {
+        setBlockNumber(await alchemy.core.getBlockNumber());
+      }
     }
 
     getBlockNumber();
   });
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+  useEffect(() => {
+    async function getTransactions() {
+        const { transactions } = await alchemy.core.getBlockWithTransactions(blockNumber)
+        setBlockTransactions(transactions);
+    }
+    getTransactions();
+  }, [blockNumber]);
+
+  const previousBlock = () => {
+    const actualBlocknumber = blockNumber - 1 < 0 ? 0 : blockNumber - 1 
+    setBlockNumber(actualBlocknumber)
+  }
+
+  const nextBlock = () => {
+    const actualBlocknumber = blockNumber + 1 
+    setBlockNumber(actualBlocknumber)
+  }
+
+  const substring = (value, index) => {
+    if (value.lenght < index) {
+      return value;
+    }
+    return value.substring(0, index) + '...';
+  }
+
+  const calculateFee = (limit, price, toFixed) => {
+    const gasFee = limit * price;
+
+    if(gasFee.toString() === "NaN") {
+      return "0";
+    }
+    
+    return parseFloat(Utils.formatEther(gasFee.toString())).toFixed(toFixed);
+  }
+
+  const Block = () => {
+    return (
+      <>
+        <div style={{
+            display:"flex", 
+            justifyContent:"center", 
+            marginTop: 24, 
+            fontSize: 24,
+          }}>
+          <b>Block Details</b>
+        </div>
+        <div style={{display:"flex", justifyContent: "center", marginTop: 24}}>
+          <button style={{marginRight: 24}} onClick={() => previousBlock()}>Previous Block</button>
+          <p>{` Block Number: ${blockNumber} `}</p>
+          <button style={{marginLeft: 24}}onClick={() => nextBlock()}>Next Block</button>
+        </div>
+      </>
+    );
+  }
+
+  const Transactions = () => {
+    return (
+      <>
+        <table>
+          <thead>
+            <tr>
+              <th>Transaction Hash</th>
+              <th>Block</th>
+              <th>From</th>
+              <th>To</th>
+              <th>Confirmations</th>
+              <th>Value</th>
+              <th>Transaction Fee</th>
+            </tr>
+          </thead>
+          <tbody>
+            {blockTransactions.map(transaction => {
+              return (
+                <tr key={transaction.hash}>
+                  <td >{substring(transaction.hash, 32)}</td>
+                  <td>{transaction.blockNumber}</td>
+                  <td>{transaction.from}</td>
+                  <td>{transaction.to}</td>
+                  <td>{transaction.confirmations}</td>
+                  <td>{Utils.formatEther(transaction.value.toString())}</td>
+                  <td>{calculateFee(transaction.gasLimit, transaction.gasPrice, 6)}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+
+
+  return <div className="App">
+    <Block />
+    <Transactions />
+  </div>;
 }
 
 export default App;
